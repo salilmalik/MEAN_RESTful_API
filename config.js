d1c91d80069f52ddcbd1dd5560c2cd69ea7f185f
Malik,Salil <salilmalik92@gmail.com>
2016-01-13 00:25:54 +0530

    Change of Port

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-'port': process.env.PORT || 8080,
+'port': process.env.PORT || 8000,
 'database': 'mongodb://meanrestfulapi:kuchbhi77@ds029615.mongolab.com:29615/meanrestfulapi' 
 };
