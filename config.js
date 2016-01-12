fdbe9f9c82cfd7e90d67d10c72ae44e830c5b28b
Malik,Salil <salilmalik92@gmail.com>
2016-01-13 00:47:22 +0530

    Changed port back to 8080

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-'port': process.env.PORT || 8000,
+'port': process.env.PORT || 8080,
 'database': 'mongodb://meanrestfulapi:kuchbhi77@ds029615.mongolab.com:29615/meanrestfulapi' 
 };
